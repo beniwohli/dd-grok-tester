168ed69f0234b864528099c1a2452c33f36c37d3
Benjamin Wohlwend <benjamin.wohlwend@twint.ch>
2026-07-01 10:48:09 +0200

    fix: replace deprecated Math.random ID with crypto.randomUUID

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,4 +1,4 @@
-export const generateId = () => Math.random().toString(36).substr(2, 9);
+export const generateId = () => crypto.randomUUID();
 
 /**
  * Splits a single rule line of the form "NAME PATTERN" into its constituent
